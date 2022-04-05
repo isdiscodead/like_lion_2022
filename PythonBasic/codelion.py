fbfdcc3a98bd1c2884f004bde27038355d0d5592
isdiscodead <isdiscodead@korea.ac.kr>
2022-04-05 13:24:52 +0900

    python dict

--- PythonBasic/codelion.py
+++ PythonBasic/codelion.py
@@ -1,6 +1,14 @@
-import random
-
-lunch = random.choice(["된장찌개","피자","제육볶음"])
-lunch = "냉장고"
-dinner = random.choice(["김밥","쫄면","돈까스"])
-print(lunch)
+information = {"고향":"수원", "취미":"영화관람","좋아하는 음식":"국수"}
+foods = ["된장찌개", "피자", "제육볶음"]
+print(information.get("취미"))
+information["특기"] = "피아노"
+information["사는곳"] = "서울"
+del information["좋아하는 음식"]
+print(information)
+print(len(information))
+information.clear()
+print(information)
+print(foods[-2])
+foods.append("김밥")
+del foods[1]
+print(foods)
