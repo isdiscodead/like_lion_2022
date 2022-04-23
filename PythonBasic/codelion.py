8238bd1dd794d4e2a16f94ebfc963b16082510e1
isdiscodead <isdiscodead@korea.ac.kr>
2022-04-24 00:29:34 +0900

    python basic #8

--- PythonBasic/codelion.py
+++ PythonBasic/codelion.py
@@ -1,14 +1,18 @@
-information = {"고향":"수원", "취미":"영화관람","좋아하는 음식":"국수"}
+for x in range(30):
+    print(x)
+
 foods = ["된장찌개", "피자", "제육볶음"]
-print(information.get("취미"))
-information["특기"] = "피아노"
-information["사는곳"] = "서울"
-del information["좋아하는 음식"]
-print(information)
-print(len(information))
-information.clear()
-print(information)
-print(foods[-2])
-foods.append("김밥")
-del foods[1]
-print(foods)
+
+for x in range(3):
+    print(foods[x])
+
+# 리스트의 개수에 상관 없이 접근 가능하도록
+for x in foods:
+    print(x)
+
+
+information = {'고향':'수원', '취미':'영화 관람', '좋아하는 음식':'국수'}
+
+# Dictionary 순차 접근 시 .items() 사용
+for x, y in information.items():
+    print(x, ":", y)
