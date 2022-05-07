e46b7c51ef33fc7e54d5ac4626d42a92b625334f
isdiscodead <isdiscodead@korea.ac.kr>
2022-05-08 01:47:51 +0900

    jum me chu end

--- PythonBasic/JumMeChu.py
+++ PythonBasic/JumMeChu.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 lunch = ["된장찌개", "피자", "제육볶음", "짜장면"]
 
 while True : 
@@ -12,3 +15,28 @@ print(lunch)
 
 # 중복 제거 
 set_lunch = set(lunch)
+
+# 음식 삭제 
+while True:
+    print(set_lunch)
+    item = input("음식을 삭제해주세요 : ")
+    if(item == "q"):
+        break
+    else:
+        set_lunch = set_lunch - set([item])
+
+# 선택 대기 ...
+print(set_lunch, "중에서 선택합니다.")
+print("5")
+time.sleep(1)
+print("4")
+time.sleep(1)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+time.sleep(1)
+
+# 선택
+print(random.choice(list(set_lunch)))
