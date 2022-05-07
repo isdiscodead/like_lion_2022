53a2abc432c88867f63073e9c8e6dd9f8f96581a
isdiscodead <isdiscodead@korea.ac.kr>
2022-05-08 01:37:01 +0900

    python basic project #3

--- PythonBasic/JumMeChu.py
+++ PythonBasic/JumMeChu.py
@@ -1,6 +1,14 @@
 lunch = ["된장찌개", "피자", "제육볶음", "짜장면"]
 
 while True : 
-    item = input("음식을 추가해주세요: ")
-    lunch.append(input())
     print(lunch)
+    item = input("음식을 추가해주세요: ")
+    if ( item == "q") :
+        break
+    else :
+        lunch.append(item)
+
+print(lunch)
+
+# 중복 제거 
+set_lunch = set(lunch)
