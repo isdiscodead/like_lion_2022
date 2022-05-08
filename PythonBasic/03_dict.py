fd7956f6d0f6547544f4ca4023baf24916409843
isdiscodead <isdiscodead@korea.ac.kr>
2022-05-08 18:03:37 +0900

    basic end

--- PythonBasic/03_dict.py
+++ PythonBasic/03_dict.py
@@ -1,10 +1,15 @@
-total_dictionary = {}
+total_list = []
 
 while True:
     question = input("질문을 입력해주세요 : ")
     if question == "q":
         break
     else:
-        total_dictionary[question] = ""
-        
-print(total_dictionary)
+        total_list.append({"질문" : question, "답변" : ""})
+
+for i in total_list:
+    print(i["질문"])
+    answer = input("답변을 입력해주세요 : ")
+    i["답변"] = answer
+    
+print(total_list)
