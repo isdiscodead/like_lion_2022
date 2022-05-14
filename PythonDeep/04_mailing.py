737c365cdf599727bd290c47aab7fff7b6a1ce40
isdiscodead <isdiscodead@korea.ac.kr>
2022-05-15 04:24:57 +0900

    python deep end

--- PythonDeep/04_mailing.py
+++ PythonDeep/04_mailing.py
@@ -38,7 +38,6 @@ message.set_content("코드라이언 수업 중입니다.") # 본문 내용
 message.add_attachment(image_file, maintype="image", subtype=image_type)
 
 
-
 # MIME에는 header가 존재 -> 제목, 수신/발신인 정보 등이 담김
 message["Subject"] = "이것은 제목입니다."
 message["From"] = "###@gmail.com"
@@ -47,5 +46,21 @@ message["To"] = "$$$@gmail.com"
 
 # smtp 서버에 로그인 -> 본인 계정 정보 입력
 smtp.login("###@gmail.com", "PASSWORD") 
-smtp.send_message(message) # MIME 형태의 메시지 전송
+
+
+# 메일이 유효할 경우 전송 ( 유효성 검사 )
+def sendEmail(addr) : 
+    # 정규 표현식 사용을 위한 모듈
+    import re
+    reg = "^[a-zA-Z0-9.+_-]+@[a-zA-Zo-9]+\.[a-zAZ]{2,3}$"
+    if bool(re.match(reg, "codelion.example@gmail.com") ) : # 표현식과 매치될 경우 string 반환 -> bool로 변환 
+        smtp.send_message(message) # MIME 형태의 메시지 전송
+        print("정상적으로 메일이 발송되었습니다. ")
+    else :
+        print("유효한 메일 주소가 아닙니다.")
+
+
+# 메일 전송
+sendEmail(message["To"])
+
 smtp.quit() # 서버 닫기 
